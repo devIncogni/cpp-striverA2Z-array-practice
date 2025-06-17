02a74b26beca57dd05b35ca8d7aa4aac50fe26dd
devIncogni <arinkumar10@outlook.com>
2025-06-17 23:57:10 +0530

    wrote function to find the largest number in array

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
-#include "helloworld.h"
+#include <climits>
+#include <iostream>
+#include <vector>
+
+int largestNumInArray(std::vector<int> array, int max, int pointer) {
+  if (pointer == array.size()) {
+    return max;
+  }
+  if (array[pointer] >= max) {
+    max = array[pointer];
+  }
+
+  return largestNumInArray(array, max, pointer + 1);
+}
+
+int secondLargest(std::vector<int> array) {
+
+  int largest{INT_MIN};
+  int secondLargest;
+
+  for (auto it : array) {
+    
+  }
+
+}
 
 int main() {
-  print("Hello World");
+  std::cout << "Welcome to array practice. All Solutions to Array problems in "
+               "Strivers A2Z DSA Sheet\n";
+
+  std::vector<int> array{1, 5, 4, 7, 9, 3, 6, 4, -5};
+  std::cout << largestNumInArray(array, array[0], 0);
+  std::cout << '\n';
   return 0;
 }
