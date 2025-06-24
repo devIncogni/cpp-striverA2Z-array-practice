a679e09014ba2fc55887104c135005c961520b54
devIncogni <arinkumar10@outlook.com>
2025-06-24 21:06:40 +0530

    Used hashing to optimize runtime from O(n^2) in bruteforce to O(n) in hashing

--- src/medium_lv2.cpp
+++ src/medium_lv2.cpp
@@ -1,24 +1,35 @@
 #include "medium_lv2.h"
+#include <unordered_map>
 #include <utility>
 
 std::string twoSumV1(std::vector<int> array, int sum) {
 
+  std::unordered_map<int, int> valueKey;
   for (int i{}; i < array.size(); ++i) {
-    for (int j{i + 1}; j < array.size(); ++j) {
-      if (array[i] + array[j] == sum) {
-        return "YES";
-      }
+
+    if (valueKey.find(array[i]) == valueKey.end()) {
+      valueKey[array[i]] = i;
+    }
+    int rem = sum - array[i];
+
+    if (valueKey.find(rem) != valueKey.end()) {
+      return "YES";
     }
   }
   return "NO";
 }
 
 std::pair<int, int> twoSumV2(std::vector<int> array, int sum) {
+  std::unordered_map<int, int> valueKey;
   for (int i{}; i < array.size(); ++i) {
-    for (int j{i + 1}; j < array.size(); ++j) {
-      if (array[i] + array[j] == sum) {
-        return {i, j};
-      }
+
+    if (valueKey.find(array[i]) == valueKey.end()) {
+      valueKey[array[i]] = i;
+    }
+    int rem = sum - array[i];
+
+    if (valueKey.find(rem) != valueKey.end()) {
+      return {valueKey[rem], i};
     }
   }
   return {-1, -1};
